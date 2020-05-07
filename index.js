8aeef78d7c939afc5327276b90bddf6007cb8649
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-05-07 18:13:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -72,13 +72,10 @@ function createRock(x) {
      }
   }
 
-  // We should kick off the animation of the rock around here.
 window.requestAnimationFrame(moveRock)
-  // Add the rock to ROCKS so that we can remove all rocks
-  // when there's a collision.
+
   ROCKS.push(rock)
 
-  // Finally, return the rock element you've created.
   return rock
 }
 
