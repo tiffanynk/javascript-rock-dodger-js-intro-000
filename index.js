6573a2e0f15a7cc040ce54427c956a1ece0334ce
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-05-07 18:13:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ function createRock(x) {
      if (checkCollision(rock)) {
        return endGame()
      }
-    
+
      if (top < GAME_HEIGHT) {
        window.requestAnimationFrame(moveRock)
      } else {
