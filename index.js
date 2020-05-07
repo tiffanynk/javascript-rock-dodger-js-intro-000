64d9fe608212f1d31bee25846ae1ddf9201e4db3
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-05-07 18:38:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -101,11 +101,13 @@ function moveDodger(e) {
 }
 
 function moveDodgerLeft() {
-  // implement me!
-  /**
-   * This function should move DODGER to the left
-   * (mabye 4 pixels?). Use window.requestAnimationFrame()!
-   */
+  window.requestAnimationFrame( () => {
+    let left = positionToInteger(DODGER.style.left)
+
+    if (left > 0) {
+      DODGER.style.left = `${left - 4}`
+    }
+  }
 }
 
 function moveDodgerRight() {
